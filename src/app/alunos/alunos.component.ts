1b2c6414ccb127f9b2d3c7d90685bbc3498a4be1
reddingtom <111653270+reddingtom@users.noreply.github.com>
2023-05-31 15:13:25 -0300

    Update Files
    Atualizando arquivos na aula 31-05-2023

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicosService } from '../service/servicos.service';
+import { AlunoModel } from './aluno.model';
 
 @Component({
   selector: 'app-alunos',
@@ -7,11 +8,24 @@ import { ServicosService } from '../service/servicos.service';
   styleUrls: ['./alunos.component.css'],
 })
 export class AlunosComponent implements OnInit {
+  aluno: AlunoModel = new AlunoModel();
+
+  alunos: Array<any> = new Array();
+
   constructor(private alunoService: ServicosService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.Listar();
+  }
 
   Listar() {
-    this.alunoService.listarAlunos().subscribe();
+    this.alunoService.listarAlunos().subscribe(
+      (alunos) => {
+        this.alunos = alunos;
+      },
+      (err) => {
+        console.log('Erro ao listar alunos', err);
+      }
+    );
   }
 }
